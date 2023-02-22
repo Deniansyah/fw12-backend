c8894f115e24cb0817d93a48418156bcff695281
Deniansyah <deniansyah814@gmail.com>
2023-02-22 11:54:04 +0700

    init dotenv

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
-const app = express()
+require("dotenv").config();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(cors())
-app.use(morgan('dev'))
+const app = express();
 
-app.use('/', require('./src/routes'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan("dev"));
+
+app.use("/", require("./src/routes"));
 
 app.get("/", (req, res) => {
   return res.status(200).json({
     success: true,
     message: "Beckend is running well",
-  })
-})
+  });
+});
 
-app.listen(8888, () => {
-  console.log("App listening to port 8888")
-})
+const PORT = process.env.PORT;
 
+app.listen(PORT, () => {
+  console.log(`App listening to port ${PORT}`);
+});
